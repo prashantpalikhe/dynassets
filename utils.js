ccd2735b7e38f00b016ed9bce6fcca5286193a6b
Prashant Palikhe <prashpal09@gmail.com>
2019-07-18 22:50:05 +0200

    fix: cjs

--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
-export function randomHexColorCode() {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
-}
+module.exports = {
+    randomHexColorCode() {
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        return '#' + n.slice(0, 6);
+    },
 
-export function randomNumberInRange(min, max) {
-    return Math.random() * (max - min) + min;
-}
+    randomNumberInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+};
